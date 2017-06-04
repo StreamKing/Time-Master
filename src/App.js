a303333d0835c5fd53eb39f4270cea80ce232c81
Gg <trashboard.gg.sam@gmail.com>
2017-06-04 13:40:40 +0200

    added function to start and stop timer

---
+++ src/App.js
@@ -0,0 +1,31 @@
+
+import React, {Component} from 'react';
+import TopToolbar from './TopToolbar'
+import Tasks from './Tasks';
+import './App.css';
+
+class App extends Component {
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+    }
+  }
+
+  render()
+  {
+    return(
+      <div>
+        <div>
+          <TopToolbar />
+          <Tasks />
+        </div>
+        <div className="App" />
+      </div>
+    )
+  }
+}
+
+
+
+export default App;
